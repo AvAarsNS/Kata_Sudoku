612cda76f19bc93013510505bccfca779264f24c
apolderman <alex.polderman@gmail.com>
2022-10-19 13:03:06 +0200

    commit test

--- test/template.test.js
+++ test/template.test.js
@@ -1,6 +1,7 @@
 const {
     doesRowContainNumber,
-    isRowAValidSudokuRow
+    isRowAValidSudokuRow,
+    doesFirstColumnContainAllNumbers
 } = require('../src/template');
 
 describe('This is a test suite for the sudoku checker', () => {
@@ -26,4 +27,19 @@ describe('This is a test suite for the sudoku checker', () => {
             expect(isRowAValidSudokuRow([1,2,3,4,6,7,8,9,9])).toEqual(false);
         });
     });
+    describe('Does the first column contain all numbers 1-9?', () => {
+        it('Correct Sudoku contains all numbers', () => {
+            expect(doesFirstColumnContainAllNumbers([
+                [5,3,4,6,7,8,9,1,2],
+                [6,7,2,1,9,5,3,4,8],
+                [1,9,8,3,4,2,5,6,7],
+                [8,5,9,7,6,1,4,2,3],
+                [4,2,6,8,5,3,7,9,1],
+                [7,1,3,9,2,4,8,5,6],
+                [9,6,1,5,3,7,2,8,4],
+                [2,8,7,4,1,9,6,3,5],
+                [3,4,5,2,8,6,1,7,9]
+                ])).toEqual(true);
+        });
+            });
 });
